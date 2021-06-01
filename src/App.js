bec4a9f739aa3a64d3b465052fbdaafcca31b7e2
Lailz <lailaabdulraheem@gmail.com>
2021-06-01 10:22:17 +0300

    set my app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"; // import svg icons and images
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="rectangle">
+      <p className="title">TO DO LIST</p>
+      <img
+        className="pecan"
+        src="https://www.theseasonedmom.com/wp-content/uploads/2020/08/Easy-Pecan-Pie-Recipe-1.jpg"
+      />
+      <table></table>
     </div>
   );
 }
